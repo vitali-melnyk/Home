74745405307dbfa337ccf60eab1c47709b72ca16
root <root@ip-172-31-41-122.eu-west-1.compute.internal>
2020-10-12 20:54:21 +0000

    add only test

--- Docker-files/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir 1
-
-if [ $? == 0 ]
-then
-  echo "Starting nginx"
-  echo "<p>HELLO WORLD $MY_NAME FROM ENTRYPOINT</p>" > /usr/share/nginx/html/index.html
-  nginx -g "daemon off;"
-fi
