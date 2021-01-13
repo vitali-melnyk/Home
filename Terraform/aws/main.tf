d862d8fa0cd5eacf26e7609f06ec51ada39d62ae
vitali-melnyk <70641554+vitali-melnyk@users.noreply.github.com>
2021-01-14 01:00:10 +0200

    add terraform

--- Terraform/aws/main.tf
+++ Terraform/aws/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-west-2"
-  /*   access_key = "AKIARQ2A225W22THPR5Q"
-  secret_key = "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" */
 }
 resource "aws_instance" "my_rhel8_1" {
   ami           = "ami-01e78c5619c5e68b4"
